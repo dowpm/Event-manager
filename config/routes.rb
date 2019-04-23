c9f0aa24c918bd0f85a245a5982f58e28e7703c8
dowpm <mcdowpm@gmail.com>
2019-04-23 14:52:07 -0400

    add div id root in site view and pack tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'site#index'
+  
   namespace :api do
     resources :events, only: %i[index show create destroy update]
   end
