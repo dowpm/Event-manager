2a545695466476bb3e52b8bface715dfdebe72d9
dowpm <mcdowpm@gmail.com>
2019-04-23 14:04:25 -0400

    null_session to allow cross site request forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :null_session
+    # The above code ensures that if no CSRF token is provided, Rails will respond with an empty session.
 end
