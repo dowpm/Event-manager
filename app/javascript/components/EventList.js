34fbe403efe3bf254f8cf31a52c98ae449c89894
dowpm <mcdowpm@gmail.com>
2019-04-24 22:44:26 -0400

    add style

--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -4,37 +4,39 @@ import { Link } from 'react-router-dom';
 
 class EventList extends React.Component {
   renderEvents() {
-    const { events } = this.props;
+    const {activeId, events } = this.props;
     events.sort(
       (a, b) => new Date(b.event_date) - new Date(a.event_date),
     );
 
     return events.map(event => (
       <li key={event.id}>
-        <Link to={`/events/${event.id}`}>
-            {event.event_date}
-            {' - '}
-            {event.event_type}
-      </Link>
+        <Link to={`/events/${event.id}`} className={activeId === event.id ? 'active' : ''}>
+          {event.event_date}
+          {' - '}
+          {event.event_type}
+        </Link>
       </li>
     ));
   }
 
   render() {
     return (
-      <section>
+      <section className="eventList">
         <h2>Events</h2>
         <ul>{this.renderEvents()}</ul>
       </section>
     );
-  }
+  }  
 }
 
 EventList.propTypes = {
+  activeId: PropTypes.number,
   events: PropTypes.arrayOf(PropTypes.object),
 };
 
 EventList.defaultProps = {
+  activeId: undefined,
   events: [],
 };
 
