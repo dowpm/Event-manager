90fbdd91b037de59222936702fbb1e27aad99433
dowpm <mcdowpm@gmail.com>
2019-04-24 21:36:53 -0400

    add components

--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class EventList extends React.Component {
   renderEvents() {
@@ -10,9 +11,11 @@ class EventList extends React.Component {
 
     return events.map(event => (
       <li key={event.id}>
-        {event.event_date}
-        {' - '}
-        {event.event_type}
+        <Link to={`/events/${event.id}`}>
+            {event.event_date}
+            {' - '}
+            {event.event_type}
+      </Link>
       </li>
     ));
   }
